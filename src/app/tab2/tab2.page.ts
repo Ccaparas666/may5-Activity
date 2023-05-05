b3b9b635fb6552a21b543b5129f9ebce52b2082d
ccaparas666 <carmelo.caparas@lccdo.edu.ph>
2023-05-05 18:00:23 +0800

    may 5

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
-
+import { Camera, CameraResultType } from '@capacitor/camera';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  public images: { name: string; dataUrl: string } [] = [];
+  public counter = 1;
 
-  constructor() {}
+  constructor() {
+    //
+    const savedImages = localStorage.getItem('images');
 
+    //
+    if (savedImages) {
+      //
+      this.images = JSON.parse(savedImages);
+
+      //
+      const lastSequenceNumber = localStorage.getItem('lastSequenceNumber');
+
+      //
+      if (lastSequenceNumber) {
+        //
+        this.counter = parseInt(lastSequenceNumber) + 1;
+      }
+    }
+    
+  }
+
+  async TakePic(){
+    //
+    const image = await Camera.getPhoto({resultType: CameraResultType.DataUrl,});
+
+    //
+    if (image.dataUrl){
+      //
+      const name = 'img_' + this.counter.toString().padStart(3, '0')+ '_' + new Date().toISOString().split('T')[0];
+    }
+  }
 }
